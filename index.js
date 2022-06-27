de832683fb41c88b0e745f00fa26d152328ca78d
sem6703 <sem6703@yandex.ru>
2022-06-27 17:02:11 +0300

    Update index.js
    асинхронность не получилась

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+/*
+по мотивам урока
+https://www.youtube.com/watch?v=R4RhgBJpXSQ&t=17s&ab_channel=WebDevснуля.КаналАлексаЛущенко
+*/
+
 const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
 
-const token = '5519678280:AAEUQ5K1VRl9JQ6RrjK_rsgX_SrKXo'; // тут токен кторый мы получили от botFather
+const token = '5519678280:AAEUQ5K1VRl9JQ6RrjK_rsgX_SrKXo'; // тут токен который мы получили от botFather
 
-// включаем самого обота
+// включаем самого бота
 const bot = new TelegramBot(token, {polling: true});
 
 //конфиг клавиатуры
-const keybo = [
-    [
-      {
-        text: 'Хочу кота', // текст на кнопке
-        callback_data: 'moreKeks' // данные для обработчика событий
-      }
-    ],
+let keybo = [
     [
         {
-          text: 'Хочу песика',
+          text: 'Хочу песика ',
           callback_data: 'morePes'
         }
     ],
-	[
+	
+    [
         {
           text: 'Хочу DL',
           callback_data: 'DL'
@@ -30,7 +30,8 @@ const keybo = [
           text: 'Что-то непонятное',
           callback_data: 'omg'
         }
-    ]	
+    ]		
+	
   ];
 
 // обработчик события присылания нам любого сообщения
@@ -45,23 +46,28 @@ bot.on('message', (msg) => {
     });
 });
 
+
 // обработчик событий нажатий на клавиатуру
 bot.on('callback_query', (query) => {
+	count++;
+	keybo[0][0].text='Хочу песика '+count;// коррекирую надпись на кнопке
     const chatId = query.message.chat.id;
 
-    let img = '';//запрос картинки
+    let img = '';
 
     if (query.data === 'moreKeks') { // если кот
         img = 'keks.jpg';
     }
 
     if (query.data === 'morePes') { // если пёс
-        img = '603291.jpg'//
-    }
+	    img = dog[Math.random()*dog.length|0];
+	}
 	
     if (query.data === 'DL') { //
-        img = 'dl.jpg'//
+        img = 'dl.jpg'
     }	
+	
+		
 
     if (img) {
         bot.sendPhoto(chatId, img, { // прикрутим клаву
@@ -77,3 +83,38 @@ bot.on('callback_query', (query) => {
         });
     }
   });
+
+
+let dog=[];
+
+void (async()=>{
+  try {  
+		const i=await fetch('https://dog.ceo/api/breeds/image/random');
+		const t=await i.json();
+		dog.push(t['message']);	  
+	  
+  } catch (e) {
+		dog=[
+		'https://images.dog.ceo/breeds/germanshepherd/n02106662_13904.jpg', 
+		'https://images.dog.ceo/breeds/bluetick/n02088632_4313.jpg', 
+		'https://images.dog.ceo/breeds/mastiff-english/2.jpg',
+		'https://images.dog.ceo/breeds/groenendael/n02105056_4205.jpg',
+		'https://images.dog.ceo/breeds/rottweiler/n02106550_3969.jpg',		
+		'https://random.dog/175c551f-d957-4be4-a15b-79dc3f0323fe.jpg',
+		'https://random.dog/6c32c078-794b-4b95-bbc4-1f272c69486f.jpg', 
+		'https://random.dog/fd66efed-7054-4a2a-ae6b-423a52070635.jpg', 
+		'https://random.dog/c88c5e58-c122-426b-a98d-3c97341e13d8.jpg', 
+		'https://random.dog/c2ef8b9e-4ebe-4148-9684-bfa84111298f.PNG', 
+		'https://random.dog/e1311960-ea27-49a2-9789-d854f4500fea.gif',
+		'https://random.dog/cdfe24b3-8ba8-44b1-a5f4-4174936dabb6.jpg',
+		'https://random.dog/866c8bed-e040-4e05-9009-9d525e03c9f6.jpg',
+		'https://random.dog/c146850f-3420-46b6-b03a-0e4f873d8a93.jpg',
+		'https://images.dog.ceo/breeds/bluetick/n02088632_2454.jpg',
+		'https://images.dog.ceo/breeds/airedale/n02096051_5952.jpg',
+		'https://images.dog.ceo/breeds/poodle-standard/n02113799_298.jpg',
+		'https://images.dog.ceo/breeds/germanshepherd/n02106662_18405.jpg',
+		'https://images.dog.ceo/breeds/terrier-american/n02093428_3269.jpg'		 
+		];
+	  }     
+})()
+
